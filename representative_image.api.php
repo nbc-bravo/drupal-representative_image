1dab6954529391d0f68b8bc978799a9b84697250
git <git@971150.no-reply.drupal.org>
2013-12-09 19:41:44 -0500

    Issue #2153039 by Rost: Please add support for custom non-image field to be used as Representative Image.

--- representative_image.api.php
+++ representative_image.api.php
@@ -25,3 +25,38 @@
 function hook_representative_image_ENTITY_TYPE_image_alter(&$image, $entity, $bundle_name) {
 
 }
+
+
+/**
+ * Alters the array of supported field widgets.
+ *
+ * @param array $widgets
+ *    Array of supported field widgets, by default array('image_image', 'media_generic').
+ */
+function hook_representative_image_widget_type_alter(&$widgets){
+  // Field widget type to support.
+  $widgets[] = 'some_field_widget';
+}
+
+/**
+ * Allows other modules rewrite output of the 'representative image' views field.
+ *
+ * @param $type
+ *    Type of entity.
+ *
+ * @param $entity
+ *    Entity object.
+ *
+ * @param $field
+ *    The name of the field.
+ *
+ * @return $image_field
+ *    Renderable array, ready for passing to render().
+ */
+function hook_representative_image_views_handler_render($type, $entity, $field){
+  // Field type to support.
+  if ($field == 'some_field'){
+    $image_field = field_view_field($type, $entity, $field, array('type' => 'some_field_formatter', 'label' => 'hidden'));
+    return $image_field;
+  }
+}
