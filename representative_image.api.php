c5ac69eb4f0077e05353035ce855e71508dfbb2d
git <git@3326031.no-reply.drupal.org>
2016-12-09 09:43:31 -0500

    Issue #2834073 by renatog, cebasqueira: Coding Standards

--- representative_image.api.php
+++ representative_image.api.php
@@ -13,49 +13,48 @@
  * actual usage please see the code at
  * tests/representative_image_test/representative_image_test.module.
  *
- * @param  string $image
+ * @param string $image
  *   The full URL of the representative image.
- *
- * @param  object $entity
+ * @param object $entity
  *   The current entity. This is provided for context.
- *
- * @param  string $bundle_name
+ * @param string $bundle_name
  *   The name of the bundle we are dealing with. This is provided for context.
  */
 function hook_representative_image_ENTITY_TYPE_image_alter(&$image, $entity, $bundle_name) {
 
 }
 
-
 /**
  * Alters the array of supported field widgets.
  *
  * @param array $widgets
- *    Array of supported field widgets, by default array('image_image', 'media_generic').
+ *    Array of supported field widgets, by default
+ *    array('image_image', 'media_generic').
  */
-function hook_representative_image_widget_type_alter(&$widgets){
+function hook_representative_image_widget_type_alter(&$widgets) {
   // Field widget type to support.
   $widgets[] = 'some_field_widget';
 }
 
 /**
- * Allows other modules rewrite output of the 'representative image' views field.
+ * Hook hook_representative_image_views_handler_render().
  *
- * @param $type
+ * Allows other modules rewrite output of the 'representative image' views
+ * field.
+ *
+ * @param string $type
  *    Type of entity.
- *
- * @param $entity
+ * @param object $entity
  *    Entity object.
- *
- * @param $field
+ * @param string $field
  *    The name of the field.
  *
- * @return $image_field
+ * @return image_field
  *    Renderable array, ready for passing to render().
  */
-function hook_representative_image_views_handler_render($type, $entity, $field){
+function hook_representative_image_views_handler_render($type, $entity, $field) {
   // Field type to support.
-  if ($field == 'some_field'){
+  if ($field == 'some_field') {
     $image_field = field_view_field($type, $entity, $field, array('type' => 'some_field_formatter', 'label' => 'hidden'));
     return $image_field;
   }
