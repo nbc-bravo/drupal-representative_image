c7d10457897d158f835c67c13d73d4aba467b296
Juampy <juampynr@gmail.com>
2018-04-21 13:26:27 +0200

    Start porting RepresentativeImageEntitiesTest and settings form

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\representative_image\Form;
 
+use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
@@ -28,30 +29,99 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['example'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Example'),
-      '#default_value' => $this->config('representative_image.settings')->get('example'),
+    $config = $this->config('representative_image.settings');
+
+    $form['default_behavior'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Default behavior when no representative image has been selected'),
+      '#default_value' => $config->get('default_behavior'),
+      '#empty_option' => $this->t('Don\'t render an image'),
+      '#options' => [
+        'first' => $this->t('Use the first image found on the given entity'),
+        'logo' => $this->t('Use the site logo'),
+        'first_or_logo' => $this->t('Use the first image on the given entity. If no image is found, use the site logo.'),
+      ],
     ];
+
+    $form['header'] = [
+      '#markup' => $this->t('Below is the list of all entity types and their respective bundles. To override the default behavior, select a field from a specific bundle.'),
+    ];
+
+    $entity_definitions = \Drupal::entityTypeManager()->getDefinitions();
+    foreach ($entity_definitions as $entity_type) {
+      $this->addEntityFieldsToForm($form, $config, $entity_type);
+    }
+
     return parent::buildForm($form, $form_state);
   }
 
   /**
-   * {@inheritdoc}
+   * Adds fields per entity to the form.
+   *
+   * @param $form
+   * @param $config
+   * @param $entity_type
+   * @param $specific_bundles
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    if ($form_state->getValue('example') != 'example') {
-      $form_state->setErrorByName('example', $this->t('The value is not correct.'));
+  protected function addEntityFieldsToForm(&$form, $config, $entity_type, $specific_bundles = array()) {
+    $bundles = \Drupal::service('entity_type.bundle.info')
+      ->getBundleInfo($entity_type->id());
+    // If necessary, filter out the bundles.
+    $bundles = empty($specific_bundles) ? $bundles : array_intersect_key_key($bundles, array_flip($specific_bundles));
+
+    if (!$entity_type->entityClassImplements(FieldableEntityInterface::class)) {
+      return ;
     }
-    parent::validateForm($form, $form_state);
+
+    $form[$entity_type->id()] = array(
+      '#type' => 'fieldset',
+      '#title' => $entity_type->getBundleLabel(),
+      '#collapsed' => TRUE,
+      '#collapsible' => TRUE,
+      '#group' => 'entities',
+      '#attributes' => array('id' => 'representative_image_' . $entity_type->id()),
+    );
+    $form[$entity_type->id()]['header'] = array(
+      '#markup' => $this->t('Select an image field to use for each bundle.'),
+    );
+    $options = [];
+    foreach ($bundles as $bundle_id => $bundle_config) {
+      $field_definitions = \Drupal::service('entity_field.manager')->getFieldDefinitions($entity_type->id(), $bundle_id);
+      foreach ($field_definitions as $field_id => $field_definition) {
+        if ($field_definition->getType() == 'image') {
+          $options[$field_id] = $field_definition->label() . ' (' . $field_id . ')';
+        }
+      }
+      if (empty($options)) {
+        $form[$entity_type->id()][$bundle_id] = array(
+          '#type' => 'item',
+          '#title' => $bundle_config['label'],
+          '#description' => $this->t('No image fields available.'),
+          '#parents' => array('representative_image', $entity_type->id(), $bundle_id),
+        );
+      }
+      else {
+        $form[$entity_type->id()][$bundle_id] = array(
+          '#type' => 'select',
+          '#title' => $bundle_config['label'],
+          '#options' => $options,
+          '#empty_option' => $this->t('Default behavior'),
+          '#empty_value' => '',
+          '#parents' => array('representative_image', $entity_type->id(), $bundle_id),
+          '#default_value' => $config->get('entity_overrides.' . $entity_type->id() . '.' . $bundle_id),
+        );
+      }
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
     $this->config('representative_image.settings')
-      ->set('example', $form_state->getValue('example'))
+      ->set('default_behavior', $values['default_behavior'])
+      ->set('entity_overrides', $values['representative_image'])
       ->save();
     parent::submitForm($form, $form_state);
   }
--- tests/src/Functional/RepresentativeImageEntitiesTest.php
+++ tests/src/Functional/RepresentativeImageEntitiesTest.php
@@ -3,47 +3,41 @@
 namespace Drupal\Tests\representative_image\Functional;
 
 /**
- * Tests representative images in entities.
+ * Test that entities can have associated representative image fields.
  *
  * @group representative_image
  */
 class RepresentativeImageEntitiesCase extends RepresentativeImageBaseTest {
 
-  public function setUp() {
-    parent::setUp();
-  }
-
-  public static function getInfo() {
-    return array(
-      'name' => 'Representative Image functionality',
-      'description' => 'Test that entities can have associated representative image fields.',
-      'group' => 'Representative Image',
-    );
-  }
-
   /**
    * Confirm that the defaults are sensible out of the box.
    */
   public function testDefaults() {
-    // Add some image fields to the article content type.
-    $new_field_label1 = $this->createImageField('admin/structure/types/manage/article/fields');
+    /** @var \Drupal\Core\File\FileSystem $file_system */
+    $file_system = \Drupal::service('file_system');
+
+    // Create a content type and add an image field to it.
+    $field_name = strtolower($this->randomMachineName());
+    $this->drupalCreateContentType(['type' => 'article', 'name' => 'Article']);
+    $this->createImageField($field_name, 'article');
 
     // Create a test node with some images.
     $image0 = $this->randomFile('image');
     $image1 = $this->randomFile('image');
     $edit = array(
-      'title' => ($this->randomName()),
-      'files[field_image_' . LANGUAGE_NONE . '_0]' => drupal_realpath($image0->uri),
-      'files[field_' . $new_field_label1 . '_' . LANGUAGE_NONE . '_0]' => drupal_realpath($image1->uri),
+      'title[0][value]' => $this->randomMachineName(),
+      'files[' . $field_name . '_0]' => \Drupal::service('file_system')
+        ->realpath($image0->uri),
     );
-    $this->drupalPost('node/add/article', $edit, t('Save'));
+    $this->drupalPostForm('node/add/article', $edit, 'Save');
+    $this->drupalPostForm(NULL, [$field_name . '[0][alt]' => $this->randomMachineName()], t('Save'));
     $nid_with_image = $this->getIdFromPath('node');
 
     // Create a test node without any images.
     $edit = array(
-      'title' => ($this->randomName()),
+      'title[0][value]' => ($this->randomString()),
     );
-    $this->drupalPost('node/add/article', $edit, t('Save'));
+    $this->drupalPostForm('node/add/article', $edit, t('Save'));
     $nid_without_image = $this->getIdFromPath('node');
 
     // Set default to "logo" and check that it works.
@@ -59,158 +53,162 @@ class RepresentativeImageEntitiesCase extends RepresentativeImageBaseTest {
     $this->setDefaultMethod('first_or_logo');
     $this->assertTrue(representative_image(node_load($nid_without_image, TRUE), 'node') == theme_get_setting('logo'), 'The global default image out of the box is the logo.');
     $edit = array(
-      'files[field_' . $new_field_label1 . '_' . LANGUAGE_NONE . '_0]' => drupal_realpath($image1->uri),
+      'files[field_' . $new_field_label1 . '_0]' => $file_system->realpath($image1->uri),
     );
-    $this->drupalPost('node/' . $nid_without_image . '/edit', $edit, t('Save'));
+    $this->drupalPostForm('node/' . $nid_without_image . '/edit', $edit, t('Save'));
     $this->assertImage($image1, representative_image(node_load($nid_without_image), 'node'), 'The global default image out of the box is the first image from the first image field.');
 
   }
 
-  /**
-   * Confirm that node entities can have representative images.
-   */
-  public function testNodeTest() {
-    // Add the existing image field (field_image) for Page.
-    $edit = array(
-      'fields[_add_existing_field][field_name]' => 'field_image',
-      'fields[_add_existing_field][widget_type]' => 'image_image',
-      'fields[_add_existing_field][label]' => $this->randomName(),
-    );
-    $this->drupalPost('admin/structure/types/manage/page/fields', $edit, 'Save');
-    $this->drupalPost(NULL, array(), 'Save settings');
-
-    // Add a new field for Page and set the representative images for the article
-    // and page content types.
-    $new_field_label = $this->createImageField('admin/structure/types/manage/page/fields');
-    $this->setRepresentativeImageField('node', 'page', 'field_' . $new_field_label);
-    $this->setRepresentativeImageField('node', 'article', 'field_image');
-
-    // Grab some dummy images.
-    $images = array();
-    for ($i = 0; $i < 3; $i++) {
-      $images[$i] = $this->randomFile('image');
-    }
-    // Now create a new Article with a given image.
-    $edit = array(
-      'title' => $this->randomName(),
-      'files[field_image_' . LANGUAGE_NONE . '_0]' => drupal_realpath($images[0]->uri),
-    );
-    $this->drupalPost('node/add/article', $edit, t('Save'));
-
-    // Now create a new Page with two images, one per field (this will allow us
-    // to test if the correct image is returned as "representative").
-    $edit = array(
-      'title' => ($article_image = $this->randomName()),
-      'files[field_image_' . LANGUAGE_NONE . '_0]' => drupal_realpath($images[1]->uri),
-      'files[field_' . $new_field_label . '_' . LANGUAGE_NONE . '_0]' => drupal_realpath($images[2]->uri),
-    );
-    $this->drupalPost('node/add/page', $edit, t('Save'));
-
-    $this->assertRepresentativeImageField('node', 'page', 'field_' . $new_field_label, 'Page\'s representative image is a new image field (even though field_image does exist.');
-    $this->assertRepresentativeImageField('node', 'article', 'field_image');
-    $this->assertRepresentativeImage(node_load(1), 'node', $images[0]);
-    $this->assertRepresentativeImage(node_load(2), 'node', $images[2]);
-  }
-
-  /**
-   * Confirm that user entities can have representative images.
-   */
-  public function testUserTest() {
-    // Add an image field to the default user bundle and set it as the
-    // representative image. By running this process twice we test that changing
-    // the representative image field on users works properly.
-    for ($i = 1; $i <= 2; $i++) {
-      $this->resetStaticVariables();
-
-      $image = $this->randomFile('image');
-
-      $new_field_label = $this->createImageField('admin/config/people/accounts/fields');
-      $this->setRepresentativeImageField('user', 'user', 'field_' . $new_field_label);
-
-      // Add an image to the admin user.
-      $edit = array(
-        'files[field_' . $new_field_label . '_' . LANGUAGE_NONE . '_0]' => drupal_realpath($image->uri),
-      );
-
-      $this->drupalPost('user/' . $this->admin->uid . '/edit', $edit, t('Save'));
-      $this->assertRepresentativeImageField('user', 'user', 'field_' . $new_field_label);
-      $this->assertRepresentativeImage(user_load($this->admin->uid, TRUE), 'user', $image);
-    }
-  }
-
-  /**
-   * Confirm that comment entities can have representative images.
-   */
-  public function testCommentTest() {
-    $content_type = 'article';
-
-    // Add an image field to the article comment bundle and set it as the
-    // representative image. By running this process twice we test that changing
-    // the representative image field on comments works properly.
-    for ($i = 1; $i <= 2; $i++) {
-      $this->resetStaticVariables();
-
-      $image = $this->randomFile('image');
-
-      $new_field_label = $this->createImageField('admin/structure/types/manage/' . $content_type . '/comment/fields');
-      $this->setRepresentativeImageField('comment', 'comment_node_' . $content_type, 'field_' . $new_field_label);
-
-      // Add an image to a comment on a node.
-      $node = $this->drupalCreateNode(array('type' => $content_type));
-      $edit = array(
-        'comment_body[' . LANGUAGE_NONE . '][0][value]' => $this->randomString(),
-        'files[field_' . $new_field_label . '_' . LANGUAGE_NONE . '_0]' => drupal_realpath($image->uri),
-      );
-      $this->drupalPost('comment/reply/' . $node->nid, $edit, t('Save'));
-      $cid = $this->getIdFromPath('comment');
-
-      if (!empty($cid)) {
-        $this->assertRepresentativeImageField('comment', 'comment_node_' . $content_type, 'field_' . $new_field_label);
-        $this->assertRepresentativeImage(comment_load($cid, TRUE), 'comment', $image);
-      }
-
-    }
-  }
-
-  public function testFileTest() {
-    // =========================================================================
-    // Imprtant note: This test assumes file_entity-7.x-2.x-unstable7 or higher.
-    // =========================================================================
-    $file_type = 'document';
-
-    // Add an image field to the file image bundle and set it as the
-    // representative image. The language cache needs to be reset, otherwise the
-    // fields are not returned properly.
-    drupal_static_reset('field_language');
-    $file = $this->randomFile($file_type == 'document' ? 'text' : $file_type);
-    $image = $this->randomFile('image');
-
-    $new_field_label = $this->createImageField('admin/structure/file-types/manage/' .$file_type  . '/fields');
-    $this->setRepresentativeImageField('file', $file_type, 'field_' . $new_field_label);
-
-    // First create the file entity. Do not confuse this with $file.
-    $edit = array(
-      'files[upload]' => drupal_realpath($file->uri),
-    );
-    $this->drupalPost('file/add', $edit, t('Next'));
-    $this->drupalPost(NULL, array('scheme' => 'public'), t('Next'));
-    $this->drupalPost(NULL, array(), t('Save'));
-    $this->clickLink($file->filename);
-
-    $fid = $this->getIdFromPath('file');
-
-    // Now upload an image to our custom field.
-    $edit = array(
-      'files[field_' . $new_field_label . '_' . LANGUAGE_NONE . '_0]' => drupal_realpath($image->uri),
-    );
-    $this->drupalPost('file/' . $fid . '/edit', $edit, t('Save'));
-
-    // Get the full file entity.
-    $files = entity_load('file', array($fid), array(), TRUE);
-    $file = array_pop($files);
-
-    $this->assertRepresentativeImageField('file', $file_type, 'field_' . $new_field_label);
-    $this->assertRepresentativeImage($file, 'file', $image);
-  }
+//  /**
+//   * Confirm that node entities can have representative images.
+//   */
+//  public function testNodeTest() {
+//    $this->drupalCreateContentType(['type' => 'article']);
+//    $this->drupalCreateContentType(['type' => 'page']);
+//
+//    // Add the existing image field (field_image) for Page.
+//    $edit = array(
+//      'fields[_add_existing_field][field_name]' => 'field_image',
+//      'fields[_add_existing_field][widget_type]' => 'image_image',
+//      'fields[_add_existing_field][label]' => $this->randomString(),
+//    );
+//    $this->drupalPostForm('admin/structure/types/manage/page/fields', $edit, 'Save');
+//    $this->drupalPostForm(NULL, array(), 'Save settings');
+//
+//    // Add a new field for Page and set the representative images for the article
+//    // and page content types.
+//    $new_field_label = $this->createImageField('admin/structure/types/manage/page/fields');
+//    $this->setRepresentativeImageField('node', 'page', 'field_' . $new_field_label);
+//    $this->setRepresentativeImageField('node', 'article', 'field_image');
+//
+//    // Grab some dummy images.
+//    $images = array();
+//    for ($i = 0; $i < 3; $i++) {
+//      $images[$i] = $this->randomFile('image');
+//    }
+//    // Now create a new Article with a given image.
+//    $edit = array(
+//      'title' => $this->randomString(),
+//      'files[field_image_0]' => drupal_realpath($images[0]->uri),
+//    );
+//    $this->drupalPostForm('node/add/article', $edit, t('Save'));
+//
+//    // Now create a new Page with two images, one per field (this will allow us
+//    // to test if the correct image is returned as "representative").
+//    $edit = array(
+//      'title' => ($article_image = $this->randomString()),
+//      'files[field_image_0]' => drupal_realpath($images[1]->uri),
+//      'files[field_' . $new_field_label . '_0]' => drupal_realpath($images[2]->uri),
+//    );
+//    $this->drupalPostForm('node/add/page', $edit, 'Save');
+//
+//    $this->assertRepresentativeImageField('node', 'page', 'field_' . $new_field_label, 'Page\'s representative image is a new image field (even though field_image does exist.');
+//    $this->assertRepresentativeImageField('node', 'article', 'field_image');
+//    $this->assertRepresentativeImage(node_load(1), 'node', $images[0]);
+//    $this->assertRepresentativeImage(node_load(2), 'node', $images[2]);
+//  }
+//
+//  /**
+//   * Confirm that user entities can have representative images.
+//   */
+//  public function testUserTest() {
+//    // Add an image field to the default user bundle and set it as the
+//    // representative image. By running this process twice we test that changing
+//    // the representative image field on users works properly.
+//    for ($i = 1; $i <= 2; $i++) {
+//      $this->resetStaticVariables();
+//
+//      $image = $this->randomFile('image');
+//
+//      $new_field_label = $this->createImageField('admin/config/people/accounts/fields');
+//      $this->setRepresentativeImageField('user', 'user', 'field_' . $new_field_label);
+//
+//      // Add an image to the admin user.
+//      $edit = array(
+//        'files[field_' . $new_field_label . '_0]' => drupal_realpath($image->uri),
+//      );
+//
+//      $this->drupalPostForm('user/' . $this->adminUser->id(). '/edit', $edit, 'Save');
+//      $this->assertRepresentativeImageField('user', 'user', 'field_' . $new_field_label);
+//      $this->assertRepresentativeImage(user_load($this->adminUser->id(), TRUE), 'user', $image);
+//    }
+//  }
+//
+//  /**
+//   * Confirm that comment entities can have representative images.
+//   */
+//  public function testCommentTest() {
+//    $content_type = 'article';
+//    $this->drupalCreateContentType(['type' => $content_type]);
+//
+//    // Add an image field to the article comment bundle and set it as the
+//    // representative image. By running this process twice we test that changing
+//    // the representative image field on comments works properly.
+//    for ($i = 1; $i <= 2; $i++) {
+//      $this->resetStaticVariables();
+//
+//      $image = $this->randomFile('image');
+//
+//      $new_field_label = $this->createImageField('admin/structure/types/manage/' . $content_type . '/comment/fields');
+//      $this->setRepresentativeImageField('comment', 'comment_node_' . $content_type, 'field_' . $new_field_label);
+//
+//      // Add an image to a comment on a node.
+//      $node = $this->drupalCreateNode(array('type' => $content_type));
+//      $edit = array(
+//        'comment_body[und][0][value]' => $this->randomString(),
+//        'files[field_' . $new_field_label . '_0]' => drupal_realpath($image->uri),
+//      );
+//      $this->drupalPostForm('comment/reply/' . $node->id(), $edit, t('Save'));
+//      $cid = $this->getIdFromPath('comment');
+//
+//      if (!empty($cid)) {
+//        $this->assertRepresentativeImageField('comment', 'comment_node_' . $content_type, 'field_' . $new_field_label);
+//        $this->assertRepresentativeImage(comment_load($cid, TRUE), 'comment', $image);
+//      }
+//
+//    }
+//  }
+//
+//  public function testFileTest() {
+//    // =========================================================================
+//    // Important note: This test assumes file_entity-7.x-2.x-unstable7 or higher.
+//    // =========================================================================
+//    $file_type = 'document';
+//
+//    // Add an image field to the file image bundle and set it as the
+//    // representative image. The language cache needs to be reset, otherwise the
+//    // fields are not returned properly.
+//    drupal_static_reset('field_language');
+//    $file = $this->randomFile($file_type == 'document' ? 'text' : $file_type);
+//    $image = $this->randomFile('image');
+//
+//    $new_field_label = $this->createImageField('admin/structure/file-types/manage/' .$file_type  . '/fields');
+//    $this->setRepresentativeImageField('file', $file_type, 'field_' . $new_field_label);
+//
+//    // First create the file entity. Do not confuse this with $file.
+//    $edit = array(
+//      'files[upload]' => drupal_realpath($file->uri),
+//    );
+//    $this->drupalPostForm('file/add', $edit, t('Next'));
+//    $this->drupalPostForm(NULL, array('scheme' => 'public'), t('Next'));
+//    $this->drupalPostForm(NULL, array(), t('Save'));
+//    $this->clickLink($file->filename);
+//
+//    $fid = $this->getIdFromPath('file');
+//
+//    // Now upload an image to our custom field.
+//    $edit = array(
+//      'files[field_' . $new_field_label . '_' . LANGUAGE_NONE . '_0]' => drupal_realpath($image->uri),
+//    );
+//    $this->drupalPostForm('file/' . $fid . '/edit', $edit, t('Save'));
+//
+//    // Get the full file entity.
+//    $files = entity_load('file', array($fid), array(), TRUE);
+//    $file = array_pop($files);
+//
+//    $this->assertRepresentativeImageField('file', $file_type, 'field_' . $new_field_label);
+//    $this->assertRepresentativeImage($file, 'file', $image);
+//  }
 
 }
