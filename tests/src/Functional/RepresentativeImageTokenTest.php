0e6c15e8f7559cf4b026c97056a9e1370d927c2b
Juampy <juampynr@gmail.com>
2018-04-26 12:28:56 +0200

    Port token integration and tests

--- tests/src/Functional/RepresentativeImageTokenTest.php
+++ tests/src/Functional/RepresentativeImageTokenTest.php
@@ -4,22 +4,20 @@ namespace Drupal\Tests\representative_image\Functional;
 
 
 /**
- * Tests token integration.
+ * Test the token integration of representative images.
  *
  * @group representative_image
  */
 class RepresentativeImageTokenTest extends RepresentativeImageBaseTest {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['field_ui', 'file', 'image', 'node', 'representative_image'];
+
   public function setUp() {
     parent::setUp();
   }
-
-//  public static function getInfo() {
-//    return array(
-//      'name' => 'Representative Image Token Integration',
-//      'description' => 'Test the token integration of representative images.',
-//      'group' => 'Representative Image',
-//    );
-//  }
 
   /**
    * Tests custom tokens.
@@ -28,32 +26,42 @@ class RepresentativeImageTokenTest extends RepresentativeImageBaseTest {
    * expected values.
    */
   public function testTokenIntegration() {
+    /** @var \Drupal\Core\Utility\Token $token */
+    $token = \Drupal::token();
+
     // Setup articles to have some image fields.
-    $new_field_label_1 = $this->createImageField('admin/structure/types/manage/article/fields');
-    $new_field_label_2 = $this->createImageField('admin/structure/types/manage/article/fields');
+    $field_image1 = 'field_image1';
+    $field_image2 = 'field_image2';
+    $this->drupalCreateContentType(['type' => 'article', 'name' => 'Article']);
+    $this->createImageField($field_image1, 'article');
+    $this->createImageField($field_image2, 'article');
 
     $image1 = $this->randomFile('image');
     $image2 = $this->randomFile('image');
 
-    // Create an article nodes for testing.
+    // Create an article node for testing.
     $edit = array(
-      'title' => $this->randomName(),
-      'files[field_' . $new_field_label_1 . '_' . LANGUAGE_NONE . '_0]' => drupal_realpath($image1->uri),
-      'files[field_' . $new_field_label_2 . '_' . LANGUAGE_NONE . '_0]' => drupal_realpath($image2->uri),
+      'title[0][value]' => $this->randomString(),
+      'files[' . $field_image1 . '_0]' => $this->fileSystem->realpath($image1->uri),
+      'files[' . $field_image2 . '_0]' => $this->fileSystem->realpath($image2->uri),
     );
-    $this->drupalPost('node/add/article', $edit, t('Save'));
-    $node = node_load($this->getIdFromPath('node'));
+    $this->drupalPostForm('node/add/article', $edit, t('Save'));
+    $this->drupalPostForm(NULL, [
+      $field_image1 . '[0][alt]' => $this->randomMachineName(),
+      $field_image2 . '[0][alt]' => $this->randomMachineName(),
+    ], t('Save'));
+    $node = $this->nodeStorage->load($this->getIdFromPath('node'));
 
     // Confirm that the correct image is being replaced properly.
-    $this->setRepresentativeImageField('node', 'article', 'field_' . $new_field_label_1);
-    $replacement = token_replace("foo [node:representative_image] bar", array('node' => $node));
+    $this->setRepresentativeImageField('node', 'article', $field_image1);
+    $replacement = $token->replace("foo [node:representative_image] bar", ['node' => $node]);
     $this->assertImage($image1, $replacement);
     $this->assertNoImage($image2, $replacement);
 
     // Switch the representative image and confirm the representative image is
     // being replaced properly.
-    $this->setRepresentativeImageField('node', 'article', 'field_' . $new_field_label_2);
-    $replacement = token_replace("foo [node:representative_image] bar", array('node' => $node));
+    $this->setRepresentativeImageField('node', 'article', $field_image2);
+    $replacement = $token->replace("foo [node:representative_image] bar", ['node' => $node]);
     $this->assertImage($image2, $replacement);
     $this->assertNoImage($image1, $replacement);
   }
