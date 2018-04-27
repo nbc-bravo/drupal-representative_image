c27963ff50fde4a22e368b4c92c07b9f0ad4c8bb
Juampy <juampynr@gmail.com>
2018-04-27 11:17:53 +0200

    Port representative image alter test

--- tests/src/Functional/RepresentativeImageAlterTest.php
+++ tests/src/Functional/RepresentativeImageAlterTest.php
@@ -7,31 +7,23 @@ namespace Drupal\Tests\representative_image\Functional;
  *
  * @group representative_image
  */
-
-/**
- * Test that third-party modules can alter the representative images.
- */
 class RepresentativeImageAlterTest extends RepresentativeImageBaseTest {
 
-  public function setUp() {
-    parent::setUp(array('representative_image_test'));
-  }
-
-//  public static function getInfo() {
-//    return array(
-//      'name' => 'Representative Image Alter functionality',
-//      'description' => 'Test that third-party modules can alter representative images.',
-//      'group' => 'Representative Image',
-//    );
-//  }
+  public static $modules = ['field_ui', 'file', 'image', 'node', 'representative_image', 'representative_image_alter_test'];
 
   /**
    * Confirm that node entities can have representative images.
    */
   public function testAlterTest() {
     global $base_url;
-    $this->assertTrue(representative_image($this->drupalCreateNode(), 'node') == $base_url . '/page.png', 'It is possible for third party modules to alter the representative image for page.');
-    $this->assertTrue(representative_image($this->drupalCreateNode(array('type' => 'article')), 'node') == $base_url . '/article.png', 'It is possible for third party modules to alter the representative image for article.');
+    $this->drupalCreateContentType(['type' => 'article', 'name' => 'Article']);
+    $this->drupalCreateContentType(['type' => 'page', 'name' => 'Page']);
+
+    $node = $this->drupalCreateNode(['type' => 'article']);
+    $this->assertEquals($base_url . '/article.png', $this->representativeImagePicker->from($node), 'It is possible for third party modules to alter the representative image for page.');
+
+    $node = $this->drupalCreateNode(['type' => 'page']);
+    $this->assertEquals($base_url . '/page.png', $this->representativeImagePicker->from($node), 'It is possible for third party modules to alter the representative image for article.');
   }
 
 }
